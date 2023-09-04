6a73187ee829a52d1d13d502bfe231df1e509375
Bacon_Space <thestreamstats@gmail.com>
2023-09-04 19:00:17 -0400

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,23 @@
-$("document").ready(function() {
-	chrome.storage.sync.get("options", function(items) {
-		for(var i = 0; i < $(".ch").length; i++) { // Check options that are enabled.
-			$(".ch:eq(" + i + ")").prop("checked", items.options[$(".ch:eq(" + i + ")").attr("id")]);
-		}
-		
-		$(".ch").click(function() { // Update option when it's clicked.
-			items.options[$(this).attr("id")] = $(this).prop("checked");
-			chrome.storage.sync.set(items);
-		});
-	});
+$(document).ready(function() {
+  // Initialize options when the popup is opened
+  chrome.storage.sync.get("options", function(items) {
+    for (var i = 0; i < $(".ch").length; i++) {
+      var checkbox = $(".ch:eq(" + i + ")");
+      var optionId = checkbox.attr("id");
+      
+      // Check options that are enabled
+      checkbox.prop("checked", items.options[optionId]);
+      
+      // Update option when it's clicked
+      checkbox.click(function() {
+        var optionId = $(this).attr("id");
+        items.options[optionId] = $(this).prop("checked");
+        
+        // Save the updated options object
+        chrome.storage.sync.set({ options: items.options }, function() {
+          console.log("Option '" + optionId + "' updated to " + items.options[optionId]);
+        });
+      });
+    }
+  });
 });
