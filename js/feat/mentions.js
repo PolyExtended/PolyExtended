cd04bc1a13e76ed6691f2ac18cda91a94bc9279b
Bacon_Space <thestreamstats@gmail.com>
2023-09-04 19:12:01 -0400

    Update mentions.js

--- js/feat/mentions.js
+++ js/feat/mentions.js
@@ -1,8 +1,20 @@
-onMessageAdd(function(mut, name, message, id) {
-	if(options.chatalerts) {
-		if((site == "beam" && mut.hasClass("tagged")) ||
-		(site == "twitch" && mut.find(".mentioned").length)) {
-			mut.css("background", "rgba(213, 24, 18, 0.2)");
-		}
-	}
+// Assuming these functions and variables are defined elsewhere in your code.
+function onMessageAdd(callback) {
+  // Your implementation of onMessageAdd here.
+}
+
+// Define the 'options' and 'site' variables somewhere in your code.
+var options = {
+  chatalerts: true, // Add your options here.
+};
+
+var site = "beam"; // Update 'site' with the appropriate value.
+
+onMessageAdd(function (mut, name, message, id) {
+  if (options.chatalerts) {
+    if ((site === "beam" && mut.hasClass("tagged")) ||
+      (site === "twitch" && mut.find(".mentioned").length)) {
+      mut.css("background", "rgba(213, 24, 18, 0.2)");
+    }
+  }
 });
