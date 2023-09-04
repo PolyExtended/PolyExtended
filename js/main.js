5972313e06057eb60b956bd4ad53e0b3f09189d9
Bacon_Space <thestreamstats@gmail.com>
2023-09-04 18:58:41 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-chrome.runtime.sendMessage("showicon");
+// Send a message to the extension background script to show an icon
+chrome.runtime.sendMessage({ action: "showicon" });
 
-var options;
-chrome.storage.sync.get("options", function(items) {
-	options = items.options;
-});
-chrome.storage.onChanged.addListener(function() {
-	chrome.storage.sync.get("options", function(items) {
-		options = items.options;
-	});
+// Initialize options with a default value (if needed)
+var options = {};
+
+// Function to update options
+function updateOptions() {
+  chrome.storage.sync.get("options", function (items) {
+    options = items.options || {}; // Use default value if "options" is not set
+    // You can now use the updated options variable here or in other parts of your code
+  });
+}
+
+// Initial update of options
+updateOptions();
+
+// Listen for changes in storage and update options accordingly
+chrome.storage.onChanged.addListener(function (changes, areaName) {
+  if (areaName === "sync" && changes.options) {
+    updateOptions();
+  }
 });
