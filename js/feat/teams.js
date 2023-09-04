be8641a7bb4989e78311954dcca03d391d360e7b
Bacon_Space <thestreamstats@gmail.com>
2023-09-04 19:13:41 -0400

    Update teams.js

--- js/feat/teams.js
+++ js/feat/teams.js
@@ -1,21 +1,23 @@
-var applyTeamsEvent = function() {};
-onMessageAdd(function(mut, name, message, id) {
-	applyTeamsEvent(mut, name, message, id);
+var applyTeamsEvent = function () {};
+onMessageAdd(function (mut, name, message, id) {
+  applyTeamsEvent(mut, name, message, id);
 });
 
-$.get("//poly.x10.mx/teams.json", function(teams) {
-	applyTeamsEvent = function(mut, name, message, id) {
-		if(options.teams) {
-			for(var i = 0; i < Object.keys(teams).length; i++) { // For each team...
-				if(teams[Object.keys(teams)[i]].members.indexOf(name.toLowerCase()) > -1) { // If user is in current selected team...
-					addBadge(mut, Object.keys(teams)[i], teams[Object.keys(teams)[i]].color, "//poly.x10.mx/badges/" + Object.keys(teams)[i] + ".png"); // Add team badge.
-					
-					if(teams[Object.keys(teams)[i]].color &&
-					(options.teamsnamesover || !getRanks(mut).length)) { // Add team color if there is one.
-						addColor(mut, teams[Object.keys(teams)[i]].color);
-					}
-				}
-			}
-		}
-	};
+$.get("//poly.x10.mx/teams.json", function (teams) {
+  applyTeamsEvent = function (mut, name, message, id) {
+    if (options.teams) {
+      for (var i = 0; i < Object.keys(teams).length; i++) {
+        var teamKey = Object.keys(teams)[i];
+        var team = teams[teamKey];
+
+        if (team.members.indexOf(name.toLowerCase()) > -1) {
+          addBadge(mut, teamKey, team.color, "//poly.x10.mx/badges/" + teamKey + ".png");
+
+          if (team.color && (options.teamsnamesover || !getRanks(mut).length)) {
+            addColor(mut, team.color);
+          }
+        }
+      }
+    }
+  };
 });
