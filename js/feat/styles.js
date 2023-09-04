76fb87fba248830f402fb6e1ea5b1698e1f0037c
Bacon_Space <thestreamstats@gmail.com>
2023-09-04 19:12:36 -0400

    Update styles.js

--- js/feat/styles.js
+++ js/feat/styles.js
@@ -1,51 +1,45 @@
-setInterval(function() {
-	if(site == "beam") {
-		if(options.separator && !$("#polystyle-separator").length) {
-			$("head").append(
-				"<style id='polystyle-separator'>" +
-					".message:nth-child(odd) {" +
-						"background: rgba(0, 0, 0, 0.4);" +
-					"}" +
-				"</style>"
-			);
-		} else if(!options.separator && $("#polystyle-separator").length) {
-			$("#polystyle-separator").remove();
-		}
-		
-		// if(options.darkheader && !$("#polystyle-darkheader").length) {
-		// 	$("head").append(
-		// 		"<style id='polystyle-darkheader'>" +
-		// 			".navbar.ng-scope, .footer {" +
-		// 				"background-color: rgba(0, 0, 0, 0.4);" +
-		// 			"}" +
-		// 		"</style>"
-		// 	);
-		// } else if(!options.darkheader && $("#polystyle-darkheader").length) {
-		// 	$("#polystyle-darkheader").remove();
-		// }
-	} else if(site == "twitch") {
-		if(options.separator && !$("#polystyle-separator").length) {
-			$("head").append(
-				"<style id='polystyle-separator'>" +
-					".chat-lines > .ember-view:nth-child(odd) {" +
-						"background: rgba(0, 0, 0, 0.4);" +
-					"}" +
-				"</style>"
-			);
-		} else if(!options.separator && $("#polystyle-separator").length) {
-			$("#polystyle-separator").remove();
-		}
-		
-		// if(options.darkheader && !$("#polystyle-darkheader").length) {
-		// 	$("head").append(
-		// 		"<style id='polystyle-darkheader'>" +
-		// 			".navbar.ng-scope, .footer {" +
-		// 				"background-color: rgba(0, 0, 0, 0.4);" +
-		// 			"}" +
-		// 		"</style>"
-		// 	);
-		// } else if(!options.darkheader && $("#polystyle-darkheader").length) {
-		// 	$("#polystyle-darkheader").remove();
-		// }
-	}
+// Assuming these variables are defined elsewhere in your code.
+var site = "beam"; // Update 'site' with the appropriate value.
+var options = {
+  separator: true, // Set your options here.
+  // darkheader: true, // You can add 'darkheader' here if needed.
+};
+
+setInterval(function () {
+  if (site === "beam" || site === "twitch") {
+    var separatorStyleId = "polystyle-separator";
+
+    if (options.separator) {
+      if (!$(`#${separatorStyleId}`).length) {
+        $("head").append(
+          `<style id='${separatorStyleId}'>` +
+            `.message:nth-child(odd) {` +
+            `background: rgba(0, 0, 0, 0.4);` +
+            `}` +
+            `</style>`
+        );
+      }
+    } else {
+      $(`#${separatorStyleId}`).remove();
+    }
+
+    // Uncomment the code below if you want to add a dark theme to the header and footer.
+    /*
+    var darkHeaderStyleId = "polystyle-darkheader";
+
+    if (options.darkheader) {
+      if (!$(`#${darkHeaderStyleId}`).length) {
+        $("head").append(
+          `<style id='${darkHeaderStyleId}'>` +
+            `.navbar.ng-scope, .footer {` +
+            `background-color: rgba(0, 0, 0, 0.4);` +
+            `}` +
+            `</style>`
+        );
+      }
+    } else {
+      $(`#${darkHeaderStyleId}`).remove();
+    }
+    */
+  }
 }, 5000);
